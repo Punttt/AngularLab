20c4adc72b55f77bccc7df353bd05b34a4635a11
Pontus Johansson <johanssonp998@gmail.com>
2026-04-21 10:13:08 +0200

    fixar all grundstruktur

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Converter } from './pages/converter/converter';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "home", component: Home },
+    { path: "about", component: About },
+    { path: "converter", component: Converter },
+    { path: "", redirectTo: "home", pathMatch: "full" },
+    { path: "404", component:NotFound },
+    { path: "**", redirectTo: "404", pathMatch: "full" }
+];
