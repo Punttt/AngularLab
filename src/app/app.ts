dd89255a24bb74836de629590b4883dea2f86972
Pontus Johansson <johanssonp998@gmail.com>
2026-04-21 15:50:08 +0200

    påbörjar med herosectionen

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainHeader } from './partials/main-header/main-header';
 import { MainFooter } from './partials/main-footer/main-footer';
+import { Hero } from './partials/hero/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainHeader, MainFooter],
+  imports: [RouterOutlet, MainHeader, MainFooter, Hero],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/partials/hero/hero.ts
+++ src/app/partials/hero/hero.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-hero',
+  selector: 'hero',
   imports: [],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
