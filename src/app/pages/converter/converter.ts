5ae441e6f1c0c13a8baa0995e6820c149ffdc9e3
Pontus Johansson <johanssonp998@gmail.com>
2026-04-22 08:49:23 +0200

    importerar forms module

--- src/app/pages/converter/converter.ts
+++ src/app/pages/converter/converter.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-converter',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './converter.html',
   styleUrl: './converter.scss',
 })
-export class Converter {}
+export class Converter {
+  // Celsius
+  celsius: number = 0;
+  resCel: number = 0;
+
+  // Fahrenheit
+  fahrenheit: number = 0;
+  resFahr: number = 0;
+
+  // Meter
+  meter: number = 0;
+  resMet: number = 0;
+
+  // Feet
+  feet: number = 0;
+  resFeet: number = 0;
+}
